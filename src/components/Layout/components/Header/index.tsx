ab5924bc8d9302d72382429818c1a1f262bdb67e
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-08 17:34:20 +0700

    add logo svg && add package Fontawesome

--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import className from 'classnames/bind'
-import styles from './Header.module.scss'
+import React from 'react';
+import className from 'classnames/bind';
+import styles from './Header.module.scss';
+import { images } from '../../../../assets/images';
+console.log(images.logo);
+const cx = className.bind(styles);
 
+const Header: React.FC = () => {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok"  />
+                </div>
+                {/* Search */}
+                <div className={cx('search')}>
+                  <input 
+                  placeholder="Search accounts and videos" 
+                  spellCheck={false}
+                  
+                  />
+                  <button className ={cx('clear')}>
+                    {/* Clear */}
+                  </button>
+                  
+                  {/* Loading */}
+                  
+                  <button className={cx('search-btn')}>
+                    {/* Search */}
+                  </button>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-const cx = className.bind(styles)
-
-const Header : React.FC = () => {
-  return (
-    <header className={cx('wrapper')}>
-      <div className={cx('inner')}>
-        {/* Logo */}
-
-        {/* Search */}
-      </div>
-    </header>
-  )
-}
-
-export default Header
+export default Header;
