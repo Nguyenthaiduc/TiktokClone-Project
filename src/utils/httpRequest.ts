ffad63ca8d8d767dabbdb9c0c08f789105285c09
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-27 20:17:51 +0700

    add environment config env

---
+++ src/utils/httpRequest.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+console.log(import.meta.env.VITE_APP_BASE_URL)
+
+const httpRequest = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
+});
+//Thông thường dùng axios res.data.data => custom res.data
+export const get  = async (path: string, option = {}) => {
+    try {
+        const response = await httpRequest.get(path,option);
+        return response.data;
+
+    }catch (err) {
+        throw new Error("Failed get data request")
+    }
+}
+
+export default httpRequest;
+
+// Local / development
+// Test / Staging
+// UAT
+// Production
