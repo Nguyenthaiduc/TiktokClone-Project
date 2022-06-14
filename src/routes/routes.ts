63c5d388cae1764e8faeb6afa49af0b0e42aed54
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-14 21:43:06 +0700

    fix fileUrl in routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
-import config from '../config';
+import config from '@/config';
 //Layouts
-import { HeaderOnly } from '../layouts';
-import { DefaultLayout } from '../layouts';
+import { HeaderOnly } from '@/layouts';
+import { DefaultLayout } from '@/layouts';
 //Pages
-import Home from '../pages/Home';
+import Home from '@/pages/Home';
 import Following from '../pages/Following';
-import Profile from '../pages/Profile';
-import Upload from '../pages/Upload';
-import Search from '../pages/Search';
+import Profile from '@/pages/Profile';
+import Upload from '@/pages/Upload';
+import Search from '@/pages/Search';
 import Live from '@/pages/Live'
 //public routes
 const publicRoutes = [
