454dd3ce1de690deec719c76c0c94f1ec4319cf1
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-17 07:59:48 +0700

    fix path Page

--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -152,8 +152,8 @@ const Header: React.FC = () => {
                         {currentUser ? (
                             <Image
                                 className={cx('user-avatar')}
-                                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_100x100.jpeg?x-expires=1652191200&x-signature=AvpOtLJwNv4XtKGB8zX5M2HHeBI%3D"
-                                alt=""
+                                src="https://i.imgur.com/7uo2Ctr.jpg"
+                                alt="Logo Username"
                                 fallback="https://avatars.githubusercontent.com/u/73944631?v=4"
                             />
                         ) : (
