db89ee302881f02b52a190841dc06489780c9343
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-07-05 06:57:43 +0700

    change func -> class AccountItem

--- src/components/AccountItem/AccountItem.tsx
+++ src/components/AccountItem/AccountItem.tsx
@@ -7,32 +7,39 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import Image from '@/components/Image';
 import { DataApi,Data } from '@/types';
+import { Component } from 'react';
 
 const cx = classNames.bind(styles);
 type Props = {
     data?: Data | any
 }
+type State = {}
 
 // Modal tìm kiếm sẽ hiện danh sách Accounts
-// Remove React.FC from Typescript template
-const AccountItem  = ({ data }: Props): JSX.Element => {
-    return (
-        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src={data.avatar}
-                alt={data.full_name}
-            />
-            <div className={cx('info')}>
-                <h4 className={cx('name')}>
-                    <span>{data.full_name}</span>
-                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle as IconProp} /> }
-                    
-                </h4>
-                <span className={cx('username')}>{data.nickname}</span>
-            </div>
-        </Link>
-    );
-};
+
+class AccountItem extends Component<Props,State> {
+    state: State = {};
+    render() {
+        return (
+                <Link to={`/@${this.props.data.nickname}`} className={cx('wrapper')}>
+                    <Image
+                        className={cx('avatar')}
+                        src={this.props.data.avatar}
+                        alt={this.props.data.full_name}
+                    />
+                    <div className={cx('info')}>
+                        <h4 className={cx('name')}>
+                            <span>{this.props.data.full_name}</span>
+                            {this.props.data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle as IconProp} /> }
+                            
+                        </h4>
+                        <span className={cx('username')}>{this.props.data.nickname}</span>
+                    </div>
+                </Link>
+            );
+    }
+}
+
+
 
 export default AccountItem;
